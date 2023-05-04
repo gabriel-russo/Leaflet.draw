050b03f8e6a3287d9433b14f48de59264a011ada
Gabriel Russo <gabrielrusso@protonmail.com>
2023-05-04 11:45:15 -0400

    Code Upgrade - Control.Draw.js

--- src/Control.Draw.js
+++ src/Control.Draw.js
@@ -6,21 +6,21 @@ L.Control.Draw = L.Control.extend({
 
 	// Options
 	options: {
-		position: 'topleft',
+		position: "topleft",
 		draw: {},
-		edit: false
+		edit: false,
 	},
 
 	// @method initialize(): void
 	// Initializes draw control, toolbars from the options
-	initialize: function (options) {
-		if (L.version < '0.7') {
-			throw new Error('Leaflet.draw 0.2.3+ requires Leaflet 0.7.0+. Download latest from https://github.com/Leaflet/Leaflet/');
+	initialize(options) {
+		if (L.version < "0.7") {
+			throw new Error("Leaflet.draw 0.2.3+ requires Leaflet 0.7.0+. Download latest from https://github.com/Leaflet/Leaflet/");
 		}
 
 		L.Control.prototype.initialize.call(this, options);
 
-		var toolbar;
+		let toolbar;
 
 		this._toolbars = {};
 
@@ -31,7 +31,7 @@ L.Control.Draw = L.Control.extend({
 			this._toolbars[L.DrawToolbar.TYPE] = toolbar;
 
 			// Listen for when toolbar is enabled
-			this._toolbars[L.DrawToolbar.TYPE].on('enable', this._toolbarEnabled, this);
+			this._toolbars[L.DrawToolbar.TYPE].on("enable", this._toolbarEnabled, this);
 		}
 
 		if (L.EditToolbar && this.options.edit) {
@@ -40,21 +40,21 @@ L.Control.Draw = L.Control.extend({
 			this._toolbars[L.EditToolbar.TYPE] = toolbar;
 
 			// Listen for when toolbar is enabled
-			this._toolbars[L.EditToolbar.TYPE].on('enable', this._toolbarEnabled, this);
+			this._toolbars[L.EditToolbar.TYPE].on("enable", this._toolbarEnabled, this);
 		}
-		L.toolbar = this; //set global var for editing the toolbar
+		L.toolbar = this; // set global var for editing the toolbar
 	},
 
 	// @method onAdd(): container
 	// Adds the toolbar container to the map
-	onAdd: function (map) {
-		var container = L.DomUtil.create('div', 'leaflet-draw'),
-			addedTopClass = false,
-			topClassName = 'leaflet-draw-toolbar-top',
-			toolbarContainer;
+	onAdd(map) {
+		let container = L.DomUtil.create("div", "leaflet-draw");
+		let addedTopClass = false;
+		let topClassName = "leaflet-draw-toolbar-top";
+		let toolbarContainer;
 
-		for (var toolbarId in this._toolbars) {
-			if (this._toolbars.hasOwnProperty(toolbarId)) {
+		for (let toolbarId in this._toolbars) {
+			if (Object.prototype.hasOwnProperty.call(this._toolbars, toolbarId)) {
 				toolbarContainer = this._toolbars[toolbarId].addToolbar(map);
 
 				if (toolbarContainer) {
@@ -76,9 +76,9 @@ L.Control.Draw = L.Control.extend({
 
 	// @method onRemove(): void
 	// Removes the toolbars from the map toolbar container
-	onRemove: function () {
-		for (var toolbarId in this._toolbars) {
-			if (this._toolbars.hasOwnProperty(toolbarId)) {
+	onRemove() {
+		for (let toolbarId in this._toolbars) {
+			if (Object.prototype.hasOwnProperty.call(this._toolbars, toolbarId)) {
 				this._toolbars[toolbarId].removeToolbar();
 			}
 		}
@@ -86,33 +86,26 @@ L.Control.Draw = L.Control.extend({
 
 	// @method setDrawingOptions(options): void
 	// Sets options to all toolbar instances
-	setDrawingOptions: function (options) {
-		for (var toolbarId in this._toolbars) {
+	setDrawingOptions(options) {
+		for (let toolbarId in this._toolbars) {
 			if (this._toolbars[toolbarId] instanceof L.DrawToolbar) {
 				this._toolbars[toolbarId].setOptions(options);
 			}
 		}
 	},
 
-	_toolbarEnabled: function (e) {
-		var enabledToolbar = e.target;
+	_toolbarEnabled(e) {
+		let enabledToolbar = e.target;
 
-		for (var toolbarId in this._toolbars) {
+		for (let toolbarId in this._toolbars) {
 			if (this._toolbars[toolbarId] !== enabledToolbar) {
 				this._toolbars[toolbarId].disable();
 			}
 		}
-	}
+	},
 });
 
 L.Map.mergeOptions({
 	drawControlTooltips: true,
-	drawControl: false
-});
-
-L.Map.addInitHook(function () {
-	if (this.options.drawControl) {
-		this.drawControl = new L.Control.Draw();
-		this.addControl(this.drawControl);
-	}
+	drawControl: false,
 });
