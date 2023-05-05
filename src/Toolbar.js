13c03aa9623630b7203d44b863bd5b82bc767996
Gabriel Russo <gabrielrusso@protonmail.com>
2023-05-05 13:25:39 -0400

    Toolbar.js - Code Upgrade; Index.html used to test changes; Removed unused Files;

--- src/Toolbar.js
+++ src/Toolbar.js
@@ -48,15 +48,15 @@ L.Toolbar = L.Class.extend({
 
 	// @method initialize(options): void
 	// Toolbar constructor
-	initialize: function (options) {
+	initialize(options) {
 		L.setOptions(this, options);
 
 		this._modes = {};
 		this._actionButtons = [];
 		this._activeMode = null;
 
-		var version = L.version.split('.');
-		//If Version is >= 1.2.0
+		let version = L.version.split(".");
+		// If Version is >= 1.2.0
 		if (parseInt(version[0], 10) === 1 && parseInt(version[1], 10) >= 2) {
 			L.Toolbar.include(L.Evented.prototype);
 		} else {
@@ -66,13 +66,13 @@ L.Toolbar = L.Class.extend({
 
 	// @method enabled(): boolean
 	// Gets a true/false of whether the toolbar is enabled
-	enabled: function () {
+	enabled() {
 		return this._activeMode !== null;
 	},
 
 	// @method disable(): void
 	// Disables the toolbar
-	disable: function () {
+	disable() {
 		if (!this.enabled()) {
 			return;
 		}
@@ -82,14 +82,14 @@ L.Toolbar = L.Class.extend({
 
 	// @method addToolbar(map): L.DomUtil
 	// Adds the toolbar to the map and returns the toolbar dom element
-	addToolbar: function (map) {
-		var container = L.DomUtil.create('div', 'leaflet-draw-section'),
-			buttonIndex = 0,
-			buttonClassPrefix = this._toolbarClass || '',
-			modeHandlers = this.getModeHandlers(map),
-			i;
+	addToolbar(map) {
+		let container = L.DomUtil.create("div", "leaflet-draw-section");
+		let buttonIndex = 0;
+		let buttonClassPrefix = this._toolbarClass || "";
+		let modeHandlers = this.getModeHandlers(map);
+		let i;
 
-		this._toolbarContainer = L.DomUtil.create('div', 'leaflet-draw-toolbar leaflet-bar');
+		this._toolbarContainer = L.DomUtil.create("div", "leaflet-draw-toolbar leaflet-bar");
 		this._map = map;
 
 		for (i = 0; i < modeHandlers.length; i++) {
@@ -113,7 +113,7 @@ L.Toolbar = L.Class.extend({
 		this._lastButtonIndex = --buttonIndex;
 
 		// Create empty actions part of the toolbar
-		this._actionsContainer = L.DomUtil.create('ul', 'leaflet-draw-actions');
+		this._actionsContainer = L.DomUtil.create("ul", "leaflet-draw-actions");
 
 		// Add draw and cancel containers to the control container
 		container.appendChild(this._toolbarContainer);
@@ -124,10 +124,10 @@ L.Toolbar = L.Class.extend({
 
 	// @method removeToolbar(): void
 	// Removes the toolbar and drops the handler event listeners
-	removeToolbar: function () {
+	removeToolbar() {
 		// Dispose each handler
-		for (var handlerId in this._modes) {
-			if (this._modes.hasOwnProperty(handlerId)) {
+		for (let handlerId in this._modes) {
+			if (Object.prototype.hasOwnProperty.call(this._modes, handlerId)) {
 				// Unbind handler button
 				this._disposeButton(
 					this._modes[handlerId].button,
@@ -140,36 +140,32 @@ L.Toolbar = L.Class.extend({
 
 				// Unbind handler
 				this._modes[handlerId].handler
-					.off('enabled', this._handlerActivated, this)
-					.off('disabled', this._handlerDeactivated, this);
+					.off("enabled", this._handlerActivated, this)
+					.off("disabled", this._handlerDeactivated, this);
 			}
 		}
 		this._modes = {};
 
 		// Dispose the actions toolbar
-		for (var i = 0, l = this._actionButtons.length; i < l; i++) {
-			this._disposeButton(
-				this._actionButtons[i].button,
-				this._actionButtons[i].callback,
-				this
-			);
+		for (let i = 0, l = this._actionButtons.length; i < l; i++) {
+			this._disposeButton(this._actionButtons[i].button, this._actionButtons[i].callback, this);
 		}
 		this._actionButtons = [];
 		this._actionsContainer = null;
 	},
 
-	_initModeHandler: function (handler, container, buttonIndex, classNamePredix, buttonTitle) {
-		var type = handler.type;
+	_initModeHandler(handler, container, buttonIndex, classNamePredix, buttonTitle) {
+		let { type } = handler;
 
 		this._modes[type] = {};
 
 		this._modes[type].handler = handler;
 
 		this._modes[type].button = this._createButton({
-			type: type,
+			type,
 			title: buttonTitle,
-			className: classNamePredix + '-' + type,
-			container: container,
+			className: `${classNamePredix}-${type}`,
+			container,
 			callback: this._modes[type].handler.enable,
 			context: this._modes[type].handler
 		});
@@ -177,24 +173,22 @@ L.Toolbar = L.Class.extend({
 		this._modes[type].buttonIndex = buttonIndex;
 
 		this._modes[type].handler
-			.on('enabled', this._handlerActivated, this)
-			.on('disabled', this._handlerDeactivated, this);
+			.on("enabled", this._handlerActivated, this)
+			.on("disabled", this._handlerDeactivated, this);
 	},
 
 	/* Detect iOS based on browser User Agent, based on:
-	 * http://stackoverflow.com/a/9039885 */
-	_detectIOS: function () {
-		var iOS = (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream);
-		return iOS;
+   * http://stackoverflow.com/a/9039885 */
+	_detectIOS() {
+		return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
 	},
 
-	_createButton: function (options) {
-
-		var link = L.DomUtil.create('a', options.className || '', options.container);
+	_createButton(options) {
+		let link = L.DomUtil.create("a", options.className || "", options.container);
 		// Screen reader tag
-		var sr = L.DomUtil.create('span', 'sr-only', options.container);
+		let sr = L.DomUtil.create("span", "sr-only", options.container);
 
-		link.href = '#';
+		link.href = "#";
 		link.appendChild(sr);
 
 		if (options.title) {
@@ -208,61 +202,62 @@ L.Toolbar = L.Class.extend({
 		}
 
 		/* iOS does not use click events */
-		var buttonEvent = this._detectIOS() ? 'touchstart' : 'click';
+		let buttonEvent = this._detectIOS() ? "touchstart" : "click";
 
-		L.DomEvent
-			.on(link, 'click', L.DomEvent.stopPropagation)
-			.on(link, 'mousedown', L.DomEvent.stopPropagation)
-			.on(link, 'dblclick', L.DomEvent.stopPropagation)
-			.on(link, 'touchstart', L.DomEvent.stopPropagation)
-			.on(link, 'click', L.DomEvent.preventDefault)
+		L.DomEvent.on(link, "click", L.DomEvent.stopPropagation)
+			.on(link, "mousedown", L.DomEvent.stopPropagation)
+			.on(link, "dblclick", L.DomEvent.stopPropagation)
+			.on(link, "touchstart", L.DomEvent.stopPropagation)
+			.on(link, "click", L.DomEvent.preventDefault)
 			.on(link, buttonEvent, options.callback, options.context);
 
 		return link;
 	},
 
-	_disposeButton: function (button, callback) {
+	_disposeButton(button, callback) {
 		/* iOS does not use click events */
-		var buttonEvent = this._detectIOS() ? 'touchstart' : 'click';
+		let buttonEvent = this._detectIOS() ? "touchstart" : "click";
 
-		L.DomEvent
-			.off(button, 'click', L.DomEvent.stopPropagation)
-			.off(button, 'mousedown', L.DomEvent.stopPropagation)
-			.off(button, 'dblclick', L.DomEvent.stopPropagation)
-			.off(button, 'touchstart', L.DomEvent.stopPropagation)
-			.off(button, 'click', L.DomEvent.preventDefault)
+		L.DomEvent.off(button, "click", L.DomEvent.stopPropagation)
+			.off(button, "mousedown", L.DomEvent.stopPropagation)
+			.off(button, "dblclick", L.DomEvent.stopPropagation)
+			.off(button, "touchstart", L.DomEvent.stopPropagation)
+			.off(button, "click", L.DomEvent.preventDefault)
 			.off(button, buttonEvent, callback);
 	},
 
-	_handlerActivated: function (e) {
+	_handlerActivated(e) {
 		// Disable active mode (if present)
 		this.disable();
 
 		// Cache new active feature
 		this._activeMode = this._modes[e.handler];
 
-		L.DomUtil.addClass(this._activeMode.button, 'leaflet-draw-toolbar-button-enabled');
+		L.DomUtil.addClass(this._activeMode.button, "leaflet-draw-toolbar-button-enabled");
 
 		this._showActionsToolbar();
 
-		this.fire('enable');
+		this.fire("enable");
 	},
 
-	_handlerDeactivated: function () {
+	_handlerDeactivated() {
 		this._hideActionsToolbar();
 
-		L.DomUtil.removeClass(this._activeMode.button, 'leaflet-draw-toolbar-button-enabled');
+		L.DomUtil.removeClass(this._activeMode.button, "leaflet-draw-toolbar-button-enabled");
 
 		this._activeMode = null;
 
-		this.fire('disable');
+		this.fire("disable");
 	},
 
-	_createActions: function (handler) {
-		var container = this._actionsContainer,
-			buttons = this.getActions(handler),
-			l = buttons.length,
-			li, di, dl, button;
+	_createActions(handler) {
+		let container = this._actionsContainer;
+		let buttons = this.getActions(handler);
+		let l = buttons.length;
+		let li;
+		let di;
+		let dl;
+		let button;
 
 		// Dispose the actions toolbar (todo: dispose only not used buttons)
 		for (di = 0, dl = this._actionButtons.length; di < dl; di++) {
@@ -275,60 +270,59 @@ L.Toolbar = L.Class.extend({
 			container.removeChild(container.firstChild);
 		}
 
-		for (var i = 0; i < l; i++) {
-			if ('enabled' in buttons[i] && !buttons[i].enabled) {
-				continue;
+		for (let i = 0; i < l; i++) {
+			if (!("enabled" in buttons[i] && !buttons[i].enabled)) {
+
+				li = L.DomUtil.create("li", "", container);
+
+				button = this._createButton({
+					title: buttons[i].title,
+					text: buttons[i].text,
+					container: li,
+					callback: buttons[i].callback,
+					context: buttons[i].context
+				});
+
+				this._actionButtons.push({
+					button,
+					callback: buttons[i].callback
+				});
 			}
-
-			li = L.DomUtil.create('li', '', container);
-
-			button = this._createButton({
-				title: buttons[i].title,
-				text: buttons[i].text,
-				container: li,
-				callback: buttons[i].callback,
-				context: buttons[i].context
-			});
-
-			this._actionButtons.push({
-				button: button,
-				callback: buttons[i].callback
-			});
 		}
 	},
 
-	_showActionsToolbar: function () {
-		var buttonIndex = this._activeMode.buttonIndex,
-			lastButtonIndex = this._lastButtonIndex,
-			toolbarPosition = this._activeMode.button.offsetTop - 1;
+	_showActionsToolbar() {
+		let { buttonIndex } = this._activeMode;
+		let lastButtonIndex = this._lastButtonIndex;
+		let toolbarPosition = this._activeMode.button.offsetTop - 1;
 
 		// Recreate action buttons on every click
 		this._createActions(this._activeMode.handler);
 
 		// Correctly position the cancel button
-		this._actionsContainer.style.top = toolbarPosition + 'px';
+		this._actionsContainer.style.top = `${toolbarPosition}px`;
 
 		if (buttonIndex === 0) {
-			L.DomUtil.addClass(this._toolbarContainer, 'leaflet-draw-toolbar-notop');
-			L.DomUtil.addClass(this._actionsContainer, 'leaflet-draw-actions-top');
+			L.DomUtil.addClass(this._toolbarContainer, "leaflet-draw-toolbar-notop");
+			L.DomUtil.addClass(this._actionsContainer, "leaflet-draw-actions-top");
 		}
 
 		if (buttonIndex === lastButtonIndex) {
-			L.DomUtil.addClass(this._toolbarContainer, 'leaflet-draw-toolbar-nobottom');
-			L.DomUtil.addClass(this._actionsContainer, 'leaflet-draw-actions-bottom');
+			L.DomUtil.addClass(this._toolbarContainer, "leaflet-draw-toolbar-nobottom");
+			L.DomUtil.addClass(this._actionsContainer, "leaflet-draw-actions-bottom");
 		}
 
-		this._actionsContainer.style.display = 'block';
+		this._actionsContainer.style.display = "block";
 		this._map.fire(L.Draw.Event.TOOLBAROPENED);
 	},
 
-	_hideActionsToolbar: function () {
-		this._actionsContainer.style.display = 'none';
+	_hideActionsToolbar() {
+		this._actionsContainer.style.display = "none";
 
-		L.DomUtil.removeClass(this._toolbarContainer, 'leaflet-draw-toolbar-notop');
-		L.DomUtil.removeClass(this._toolbarContainer, 'leaflet-draw-toolbar-nobottom');
-		L.DomUtil.removeClass(this._actionsContainer, 'leaflet-draw-actions-top');
-		L.DomUtil.removeClass(this._actionsContainer, 'leaflet-draw-actions-bottom');
+		L.DomUtil.removeClass(this._toolbarContainer, "leaflet-draw-toolbar-notop");
+		L.DomUtil.removeClass(this._toolbarContainer, "leaflet-draw-toolbar-nobottom");
+		L.DomUtil.removeClass(this._actionsContainer, "leaflet-draw-actions-top");
+		L.DomUtil.removeClass(this._actionsContainer, "leaflet-draw-actions-bottom");
 		this._map.fire(L.Draw.Event.TOOLBARCLOSED);
 	}
 });
